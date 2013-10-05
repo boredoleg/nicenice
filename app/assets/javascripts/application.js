1203458555db43889a4cd602423c9025f24a44ee
Bored Oleg <boredoleg@gmail.com>
2013-10-05 13:46:41 +0800

    Add Paperklip and JQUERY MASONRY

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//=require jquery.masonry.min.js
 //= require foundation
 //= require_tree .
 
--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,9 +1,16 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :contact_email, :contact_phone, :product_description, :product_name, :product_price
+  attr_accessible :contact_email, :contact_phone, :product_description, :product_name, :product_price, :image
 
   validates :product_name, presence: true
 	validates :product_description, presence: true
   validates :product_price, presence: true
   validates :contact_phone, presence: true
   validates :contact_email, presence: true
+  
+  has_attached_file :image, styles: { medium: "320x240>"}
+  validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }
+
+  belongs_to :user
 end
