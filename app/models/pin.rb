fcd3215d3e6a9a1e7fef2294add158a63a8e816d
Bored Oleg <boredoleg@gmail.com>
2013-10-05 12:56:32 +0800

    Adding Validations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,3 +1,9 @@
 class Pin < ActiveRecord::Base
   attr_accessible :contact_email, :contact_phone, :product_description, :product_name, :product_price
+
+  validates :product_name, presence: true
+	validates :product_description, presence: true
+  validates :product_price, presence: true
+  validates :contact_phone, presence: true
+  validates :contact_email, presence: true
 end
